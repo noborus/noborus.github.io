ef49d53480f0a1a50939502586286469e9d7e523
Noboru Saito <noborusai@gmail.com>
2019-05-04 13:03:16 +0900

    fix key path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl aes-256-cbc -K $encrypted_28f593a07933_key -iv $encrypted_28f593a07933_iv -in deploy_key.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -K $encrypted_28f593a07933_key -iv $encrypted_28f593a07933_iv -in scripts/deploy_key.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git config --global user.email "noborusai@gmail.com"
