bb855ef0b9bd961cf22bd31742bf838e571d69fb
Noboru Saito <noborusai@gmail.com>
2019-05-04 17:40:18 +0900

    umm?

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,8 @@ chmod 600 ~/.ssh/id_rsa
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git config --global user.email "noborusai@gmail.com"
 git config --global user.name "noborus"
-git remote add origin-pages git@github.com:noborus/noborus.github.io.git
+git remote set-url origin git@github.com:noborus/noborus.github.io.git
 
 git add -A current
-git commit -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER)"
-git push --set-upstream origin-pages master
+git commit --allow-empty -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER)"
+git push origin master
