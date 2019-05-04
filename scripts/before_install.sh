cafab5a5b5e373adb8b7a7b05f589150d8de3f60
Noboru Saito <noborusai@gmail.com>
2019-05-04 12:46:57 +0900

    fix deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_28f593a07933_key -iv $encrypted_28f593a07933_iv -in deploy_key.enc -out ~/.ssh/id_rsa -d
-chmod 600 ~/.ssh/id_rsa
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-git config --global user.email "noborusai@gmail.com"
-git config --global user.name "noborus"
-
 mkdir travis
-git clone --depth=1 -b doc_ja_11 git@github.com/pgsql-jp/jpug-doc.git travis/jpug-doc
+git clone --depth=1 -b doc_ja_11 https://github.com/pgsql-jp/jpug-doc.git travis/jpug-doc
 
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-cp -a travis/jpug-doc/doc/src/sgml/html current/
+openssl aes-256-cbc -K $encrypted_28f593a07933_key -iv $encrypted_28f593a07933_iv -in deploy_key.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+git config --global user.email "noborusai@gmail.com"
+git config --global user.name "noborus"
+git clone --depth=50 git@github.com/noborus/noborus.github.io.git
+
+cp -aR travis/jpug-doc/doc/src/sgml/html current/
 git add -A current
-git commit -m "update"
+git commit -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER)"
 git push origin master
