934d58013f6bbba249d21458ce0e02dd7734d674
Noboru Saito <noborusai@gmail.com>
2019-05-04 12:19:24 +0900

    add ssh-key deploy

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+openssl aes-256-cbc -K $encrypted_28f593a07933_key -iv $encrypted_28f593a07933_iv -in deploy_key.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+git config --global user.email "noborusai@gmail.com"
+git config --global user.name "noborus"
 
 mkdir travis
 git clone --depth=1 -b doc_ja_11 https://github.com/pgsql-jp/jpug-doc.git travis/jpug-doc
