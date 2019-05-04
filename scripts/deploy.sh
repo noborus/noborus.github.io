c898bb2724bcba8bfaa3fa18bd0c1b712bac05e8
Noboru Saito <noborusai@gmail.com>
2019-05-04 14:20:17 +0900

    remote add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,10 @@ chmod 600 ~/.ssh/id_rsa
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 git config --global user.email "noborusai@gmail.com"
 git config --global user.name "noborus"
-git clone --depth=50 git@github.com:noborus/noborus.github.io.git
+git remote add origin-pages git@github.com:noborus/noborus.github.io.git
 
 cp -aR travis/jpug-doc/doc/src/sgml/html current/
+cd current
 git add -A current
 git commit -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER)"
-git push origin master
+git push origin-pages master
