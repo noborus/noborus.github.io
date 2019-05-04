38ee299ef4571b2fa128dd428c87fb601f809a05
Noboru Saito <noborusai@gmail.com>
2019-05-04 11:34:14 +0900

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-mkdir current
-mv travis/jpug-doc/doc/src/sgml/html current/
+cp -a travis/jpug-doc/doc/src/sgml/html current/
 git add -A current
 git commit -m "update"
 git push origin master
