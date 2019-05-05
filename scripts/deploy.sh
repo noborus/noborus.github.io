97bc8a410475697c81c3b97a893160205b6a1750
Noboru Saito <noborusai@gmail.com>
2019-05-06 04:00:48 +0900

    by Travis CI (JOB ) [skip ci]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_28f593a07933_key -iv $encrypted_28f593a07933_iv -in scripts/deploy_key.enc -out ~/.ssh/id_rsa -d
-chmod 600 ~/.ssh/id_rsa
-echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-git config --global user.email "noborusai@gmail.com"
-git config --global user.name "noborus"
-git remote set-url origin git@github.com:noborus/noborus.github.io.git
-git checkout master
-
+if ! git diff --name-only |grep -v bookindex.html ;
+then
+    echo "OK"
+    exit 0
+fi
+echo "Update because there is a difference"
+exit 1
 git add -A current
-git commit --allow-empty -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER) [skip ci]"
-git branch
+git commit -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER) [skip ci]"
 git push ssh://git@github.com/noborus/noborus.github.io.git master:master
