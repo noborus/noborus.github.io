862fdaef9bfc5b09bbc6e2eb5b59136a267c41a4
Noboru Saito <noborusai@gmail.com>
2019-05-06 04:01:22 +0900

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,10 @@
 
 if ! git diff --name-only |grep -v bookindex.html ;
 then
-    echo "OK"
+    echo "Do not update"
     exit 0
 fi
 echo "Update because there is a difference"
-exit 1
 git add -A current
 git commit -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER) [skip ci]"
 git push ssh://git@github.com/noborus/noborus.github.io.git master:master
