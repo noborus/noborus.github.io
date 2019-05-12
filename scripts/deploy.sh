658325da817b5b05c85599dcb29aa05f12619511
Noboru Saito <noborusai@gmail.com>
2019-05-12 13:39:36 +0900

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 git add -N current
-if ! git diff --name-only current|grep -v bookindex.html ;
+if ! git diff --name-only current|grep -E -v "bookindex.html|postgres-A4.pdf" ;
 then
     echo "Do not update"
     exit 0
 fi
 
 echo "Update because there is a difference"
-sed -i -e "s/last-updated:.*$/last-updated: $(TZ=Asia/Tokyo date -u "+%Y-%m-%d %T %Z")/" index.md
+sed -i -e "s/last-updated:.*$/last-updated: $(TZ=Asia/Tokyo date "+%Y-%m-%d %T %Z")/" index.md
 git add -A current
 git commit -m "by Travis CI (JOB $TRAVIS_JOB_NUMBER) [skip ci]"
 git push ssh://git@github.com/noborus/noborus.github.io.git master:master
